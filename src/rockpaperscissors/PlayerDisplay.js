26ffe078d2c76e085d48fda186016d5edfa45ef4
jonher16 <1999.jon@gmail.com>
2022-01-12 16:35:02 +0100

    Added RSP images

--- src/rockpaperscissors/PlayerDisplay.js
+++ src/rockpaperscissors/PlayerDisplay.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const PlayerDisplay = ({img, choice, player}) => {
     return (
-        <div className="border w-50 h-100 d-inline-flex flex-column align-items-center">
-            <div className="h-50 border">
+        <div className="w-50 h-100 d-inline-flex flex-column align-items-center">
+            <div className="h-50">
            <img className="img-fluid h-100 w-100" src={img} alt="choice" />
            </div>
-           <div className=" border h-50 w-100 d-inline-flex align-items-center flex-column justify-content-center">
+           <div className=" h-50 w-100 d-inline-flex align-items-center flex-column justify-content-center">
            <h3>{player}'s choice:</h3>
            <h3> {choice}</h3>
            </div>
--- src/rockpaperscissors/RPS.js
+++ src/rockpaperscissors/RPS.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import scissors from "./img/scissors_test.png";
+import scissors from "./img/scissors.png";
 import rock from "./img/rock.png";
+import paper from "./img/paper.png"
 import PlayerDisplay from "./PlayerDisplay";
 import { Button } from "react-bootstrap";
 
@@ -9,6 +10,15 @@ const RPS = () => {
     e.preventDefault();
     console.log("Choice changed to " + choice);
     setChoice(choice);
+    if (choice === "Rock"){
+      setImage(rock)
+    }
+    else if (choice==="Scissors") {
+      setImage(scissors)
+    }
+    else if (choice==="Paper"){
+      setImage(paper)
+    }
     randomChoice();
   };
 
@@ -18,19 +28,7 @@ const RPS = () => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  function randomChoice() {
-    var random_choice = getRandomInt(1, 3);
-    if (random_choice === 1) {
-      setChoice2("Rock");
-    } else if (random_choice === 2) {
-      setChoice2("Paper");
-    } else if (random_choice === 3) {
-      setChoice2("Scissors");
-    }
-    checkWinner();
-  }
-
-  function checkWinner() {
+  const checkWinner = () => {
     if (choice === choice2) {
       setDraws(draws + 1);
     } else {
@@ -56,32 +54,49 @@ const RPS = () => {
     }
   }
 
+  function randomChoice() {
+    var random_choice = getRandomInt(1, 3);
+    if (random_choice === 1) {
+      setChoice2("Rock");
+      setImage2(rock)
+    } else if (random_choice === 2) {
+      setChoice2("Paper");
+      setImage2(paper)
+    } else if (random_choice === 3) {
+      setChoice2("Scissors");
+      setImage2(scissors)
+    }
+    checkWinner()
+  }
+
   const [choice, setChoice] = useState(" ");
   const [choice2, setChoice2] = useState("");
   const [draws, setDraws] = useState(0);
   const [wins, setWins] = useState(0);
   const [losses, setLosses] = useState(0);
+  const [image, setImage]= useState(rock)
+  const [image2, setImage2] = useState(rock)
 
   return (
-    <div className="d-inline-flex flex-column h-100 w-75 align-items-center border">
+    <div className="d-inline-flex flex-column h-100 w-75 align-items-center">
       <h1>Rock Paper Scissors</h1>
-      <div className="d-inline-flex flex-row border h-50 w-75 mt-5">
-        <PlayerDisplay img={scissors} player={"Player 1"} choice={choice} />
-        <PlayerDisplay img={scissors} player={"Computer"} choice={choice2} />
+      <div className="d-inline-flex flex-row h-50 w-75 mt-5 border border-secondary rounded pt-5">
+        <PlayerDisplay img={image} player={"Player 1"} choice={choice} />
+        <PlayerDisplay img={image2} player={"Computer"} choice={choice2} />
       </div>
       {/* Button Div */}
-      <div className="d-inline-flex flex-row justify-content-around w-50 border mt-3">
-        <Button className="w-25" onClick={(e) => handleChoice(e, "Rock")}>
+      <div className="d-inline-flex flex-row justify-content-around w-50 mt-3">
+        <Button className="btn w-25" onClick={(e) => handleChoice(e, "Rock")}>
           Rock
         </Button>
-        <Button className="w-25" onClick={(e) => handleChoice(e, "Paper")}>
+        <Button className="btn w-25" onClick={(e) => handleChoice(e, "Paper")}>
           Paper
         </Button>
-        <Button className="w-25" onClick={(e) => handleChoice(e, "Scissors")}>
+        <Button className="btn w-25" onClick={(e) => handleChoice(e, "Scissors")}>
           Scissors
         </Button>
       </div>
-      <div className="d-inline-flex flex-row justify-content-around w-100 border mt-3">
+      <div className="d-inline-flex flex-row justify-content-around w-100 mt-5">
       <h3>Wins: {wins}</h3>
       <h3>Draws: {draws}</h3>
       <h3>Losses: {losses}</h3>
