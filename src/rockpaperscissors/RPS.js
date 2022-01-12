03e6298a21a4192c1b38196d4846cf6595cc69b5
jonher16 <1999.jon@gmail.com>
2022-01-12 18:08:09 +0100

    Counters Fix

--- src/rockpaperscissors/RPS.js
+++ src/rockpaperscissors/RPS.js
@@ -6,18 +6,28 @@ import PlayerDisplay from "./PlayerDisplay";
 import { Button } from "react-bootstrap";
 
 const RPS = () => {
-  const handleChoice = (e, choice) => {
+
+  const handleChoice = (e, choice, prevclicks, prevwins, prevdraws, prevlosses) => {
+    
     e.preventDefault();
-    setClicks(clicks+1)
+    var wins = prevwins;
+    var draws = prevdraws;
+    var losses = prevlosses;
+    var clicks = prevclicks;
+
+    clicks++;
+    setClicks(clicks)
+
+
     console.log("Choice changed to " + choice);
     setChoice(choice);
-    if (choice === "Rock"){
+    if (choice === 1){
       setImage(rock)
     }
-    else if (choice==="Scissors") {
+    else if (choice=== 3) {
       setImage(scissors)
     }
-    else if (choice==="Paper"){
+    else if (choice=== 2){
       setImage(paper)
     }
 
@@ -33,32 +43,40 @@ const RPS = () => {
       setImage2(scissors)
     }
     
-    if (choice === choice2) {
-      setDraws(draws + 1);
+    if (choice === random_choice) {
+      draws++
     } else {
-      if (choice === "Rock") {
-        if (choice2 === "Scissors") {
-          setWins(wins + 1);
+      if (choice === 1) {
+        if (random_choice === 3) {
+          wins++
         } else {
-          setLosses(losses + 1);
+          losses++
         }
-      } else if (choice === "Paper") {
-        if (choice2 === "Rock") {
-          setWins(wins + 1);
+      } else if (choice === 2) {
+        if (random_choice === 1) {
+          wins++
         } else {
-          setLosses(losses + 1);
+          losses++
         }
-      } else if (choice === "Scissors") {
-        if (choice2 === "Paper") {
-          setWins(wins + 1);
+      } else if (choice === 3) {
+        if (random_choice === 2) {
+          wins++
         } else {
-          setLosses(losses + 1);
+          losses++
         }
       }
     }
-    setWinp(wins*100/clicks)
-    setLossp(losses*100/clicks)
-    setDrawp(draws*100/clicks)
+    console.log("W: ",wins,"WP: ",wins/clicks)
+    console.log("L: ",losses,"LP: ", losses/clicks)
+    console.log("D: ",draws, "DP: ", draws/clicks)
+    
+    setClicks(clicks)
+    setWins(wins)
+    setLosses(losses)
+    setDraws(draws)
+    setWinp((wins*100/clicks).toFixed(2))
+    setLossp((losses*100/clicks).toFixed(2))
+    setDrawp((draws*100/clicks).toFixed(2))
 
   };
 
@@ -69,16 +87,18 @@ const RPS = () => {
   }
 
 
-  const experiment = (e, prevdraws, prevlosses, prevwins) =>{
+  const experiment = (e, prevclicks,  prevwins, prevdraws, prevlosses, sims) =>{
     e.preventDefault()
 
-    setClicks(clicks+100)
+    var clicks = prevclicks
+    clicks+=100
+    setClicks(clicks)
 
     var draws = prevdraws;
-    console.log("prevdraws ",prevdraws)
     var wins = prevwins;
     var losses = prevlosses;
-    for(let i=0;i<100;i++){
+
+    for(let i=0;i<sims;i++){
       console.log("entre")
       var random_choice = getRandomInt(1, 3);
     if (random_choice === 1) {
@@ -129,9 +149,7 @@ const RPS = () => {
     }
     
     }
-    console.log("W: ",wins,"WP: ",wins/clicks)
-    console.log("L: ",losses,"LP: ", losses/clicks)
-    console.log("D: ",draws, "DP: ", draws/clicks)
+  
     setWins(wins)
     setLosses(losses)
     setDraws(draws)
@@ -161,17 +179,17 @@ const RPS = () => {
       </div>
       {/* Button Div */}
       <div className="d-inline-flex flex-row justify-content-around w-50 mt-3">
-        <Button className="btn w-25" onClick={(e) => handleChoice(e, "Rock")}>
+        <Button className="btn w-25" onClick={(e) => handleChoice(e, 1, clicks, wins, draws, losses)}>
           Rock
         </Button>
-        <Button className="btn w-25" onClick={(e) => handleChoice(e, "Paper")}>
+        <Button className="btn w-25" onClick={(e) => handleChoice(e, 2, clicks, wins, draws, losses)}>
           Paper
         </Button>
-        <Button className="btn w-25" onClick={(e) => handleChoice(e, "Scissors")}>
+        <Button className="btn w-25" onClick={(e) => handleChoice(e, 3, clicks, wins, draws, losses)}>
           Scissors
         </Button>
       </div>
-      <Button className="btn mt-5 w-25" onClick={(e) => experiment(e, draws, losses, wins)}>
+      <Button className="btn mt-5 w-25" onClick={(e) => experiment(e, clicks, wins, draws, losses, 100)}>
           Simulate 100
         </Button>
       <div className="d-inline-flex flex-row justify-content-around w-100 mt-5">
